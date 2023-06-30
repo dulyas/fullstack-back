54667f970cfc301516f905899d4bca26d22380d2
Kirill Kudr <94325208+dulyas@users.noreply.github.com>
2023-06-30 22:37:49 +0500

    refix message

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -57,7 +57,7 @@ export const createTodo = async (req: Request, res: Response, next: NextFunction
     try {
         const {user_id, title} = req.body
 
-        if (!user_id || !title || typeof +user_id !== 'number' || typeof title !== 'string' || title.length > 30) return next(ApiError.BadRequest('Некорректные данные, user_id: number, password: string не длиннее 30') )
+        if (!user_id || !title || typeof +user_id !== 'number' || typeof title !== 'string' || title.length > 30) return next(ApiError.BadRequest('Некорректные данные, user_id: number, title: string не длиннее 30') )
 
         const todo = await todoService.createTodo(+user_id, title)
 
